2416dc22e33f97df2ec928605e1065dc45069e99
Joao <joao.g.leal@gmail.com>
2021-03-09 18:31:35 -0300

    removed repository import

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
-import RegisteredUsersRepository from '../repositories/RegisteredUsersRepository';
 
 const usersRouter = Router();
 const usersController = new UsersController();
-const registerUsersRepository = new RegisteredUsersRepository();
 
 usersRouter.post('/', usersController.create);
 usersRouter.get('/', usersController.index);
