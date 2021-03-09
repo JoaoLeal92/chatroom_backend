41d4e5c4e029aa180ca26934150a7b52651944dd
Joao <joao.g.leal@gmail.com>
2021-03-09 06:17:56 -0300

    refactored direcotry structure

--- src/repositories/IActiveUsersRepository.ts
+++
@@ -1,6 +0,0 @@
-export default interface IActiveUsersRepository {
-  addRoom(roomId: string): void;
-  addUser(roomId: string, username: string): void;
-  removeUser(roomId: string, username: string): void;
-  getAllUsers(roomId: string): string[];
-}
